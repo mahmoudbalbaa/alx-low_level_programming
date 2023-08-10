b30cdad67197690a3fa7b94c5e15033d33436a4d
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-08-10 16:56:30 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/*
+ * File name: 5-printf.c
+ */
+
+# include <stdio.h>
+
+/*
+ * main - prints with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: Always 0 
+ */
+int main (void)
+{
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
