1da12897d77697e185abb7c7039935cbda04aecd
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-08-10 16:31:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
+/*This program about writing a string using puts function */
+
 # include <stdio.h>
 
 int main (void)
 {
-	char x [60] = "\"Programming is like building a multilingual puzzle\n";
+	char x[60] = "\"Programming is like building a multilingual puzzle";
 		
-		puts (x);
-		return (0);
+	puts(x);
+	return (0);
 }
