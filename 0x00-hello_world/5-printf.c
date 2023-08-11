b5b09f2f1c29cdeee2aaa2ee078723f6a5c2786c
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-08-10 17:15:20 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,7 @@
 
 /**
  * main - prints "with proper grammar, but the outcome 
- * 		  is a piece of art," followed by a new line
- *
+ * is a piece of art," followed by a new line
  * Return: Always 0 
  */
 int main (void)
