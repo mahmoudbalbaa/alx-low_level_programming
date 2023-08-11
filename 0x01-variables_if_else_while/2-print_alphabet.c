72ac626c39b3c0e111de38977ded041e7eee1324
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-08-11 16:31:45 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - variable char is assigned as x = 'a'
+ * WHILE x less than or equal 'z'
+ *	Prints x
+ *	Increment of x by 1
+ * Return: always 0
+ */
+int main (void)
+{
+	char x = 'a';
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return 0;
+}
