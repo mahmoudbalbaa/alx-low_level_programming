bb71450e492e46dca5637401825186a9b0762c5d
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-08-10 18:32:12 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+/*
+ * File: 101-quote.c
+ */
+
+# include <stdio.h>
+
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * 		, follwed by a new line
+ * 	Return: always 1
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
