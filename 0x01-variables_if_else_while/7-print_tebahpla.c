adf05fa445bd2e6c4e92016e912b9b64c10412ec
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-08-11 18:06:23 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - assign x = 'z' as char
+ * While x greater than or equals 'a'
+ *	Prints x
+ *	increases x by 1
+ * Puts a new line at the end
+ * Return: always 0
+ */
+int main(void)
+{
+	char x='z';
+
+	while (x >= 'a')
+	{
+		putchar(x);
+		x--;
+	}
+	putchar('\n');
+	return (0);
+}
