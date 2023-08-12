42c8ba29907a5a781329a2ce42ce84de0b485820
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-08-11 17:49:27 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - assign an int variable i =0
+ * While i less than or equals 9
+ *	Prints i
+ *	increase by 1
+ * Puts a new line after that
+ * Return: always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
