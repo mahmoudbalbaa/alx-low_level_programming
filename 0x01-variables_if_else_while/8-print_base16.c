8dc628d14ef13d362f3e596b4836cf74fce573c9
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-08-11 18:30:44 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - assigns int x = 0 in hexadecimal
+ * while x less than or equals 'f'
+ *	prints x
+ *	increases x by 1
+ * puts a new line at the end
+ * Retuen: always 0
+ */
+int main(void)
+{
+	int x = '0';
+	char z = 'a';
+
+	while (x <= '9')
+	{
+		putchar(x);
+		x++;
+	}
+	while (z <= 'f')
+	{
+		putchar(z);
+		z++;
+	}
+	putchar('\n');
+	return (0);
+}
