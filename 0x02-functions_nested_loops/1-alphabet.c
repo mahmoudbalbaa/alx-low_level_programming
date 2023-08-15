bcb8b0e5cbdbbcb5cd9853ee6141f70570187e4b
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-08-15 15:31:53 -0700

    prints the alphabits in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * void function
- * Description: Prints the alphabits in lowercase
- * return: always void
- */
+  * print_alphabet - Make the alphabet
+  *
+  * Return: void
+  */
 void print_alphabet(void)
 {
 	int a = 'a';
+
 	while (a <= 'z')
 	{
 		_putchar(a);
 		a++;
 	}
 	_putchar('\n');
-	return;
 }
