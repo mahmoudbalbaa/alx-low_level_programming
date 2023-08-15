bf5fd5b84d5df647c330489ded2545aa36c8759c
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-08-15 15:00:34 -0700

    printing _putchar using a header file contain a function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,1 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar()
-{
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-	return (0);
-}
-
-#endif
+int _putchar(char c);
