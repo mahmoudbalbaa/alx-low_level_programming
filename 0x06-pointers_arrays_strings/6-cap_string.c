d1582caa411d724b9e6aa27cc9217ad3f278f9f7
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-08-24 18:15:10 -0700

    function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,27 +3,33 @@
 
 /**
  * cap_string - function that capitalizes all words of a string
- * @a: a pointer to a char
- * Return: the value of a
+ * @s: a pointer to a char
+ * Return: the value of s
  */
 
-char *cap_string(char *a)
+char *cap_string(char *s)
 {
-	int i;
+	int count;
 
-	for (i = 0; a[i] != '\0'; i++)
+	count = 0;
+	while (s[count] != '\0')
 	{
-		if (i == 0)
-			a[i] -= 32;
-		else if (a[i] == ' ' || a[i] == '\t' || a[i] == '\n'
-			|| a[i] == ',' || a[i] == ';' || a[i] == '.'
-			|| a[i] == '!' || a[i] == '?' || a[i] == '"'
-			|| a[i] == '(' || a[i] == ')' || a[i] == '{'
-			|| a[i] == '}')
+		if (s[0] >= 97 && s[0] <= 122)
 		{
-			if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
-				a[i + 1] -= 32;
+			s[0] = s[0] - 32;
 		}
+		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+		    || s[count] == ',' || s[count] == ';' || s[count] == '.'
+		    || s[count] == '.' || s[count] == '!' || s[count] == '?'
+		    || s[count] == '"' || s[count] == '(' || s[count] == ')'
+		    || s[count] == '{' || s[count] == '}')
+		{
+			if (s[count + 1] >= 97 && s[count + 1] <= 122)
+			{
+				s[count + 1] = s[count + 1] - 32;
+			}
+		}
+		count++;
 	}
-	return (a);
+	return (s);
 }
