4a8921027d17e75cc4825c1feb705ead8b3785e8
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-09-12 11:32:26 -0700

    a new type struct dog with the following elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - Contains data of each dog
+ * @name: a pointer to char
+ * @age: a float num
+ * @owner: a pointer to char
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H */
