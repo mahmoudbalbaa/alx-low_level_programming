3c4688e9bb72a73ec64d59f937ea296b059b53f0
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-09-25 12:45:03 -0700

    update print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,13 +11,6 @@ size_t print_listint(const listint_t *h)
 	size_t n = 0;
 	const listint_t *temp = NULL;
 
-	if (h == NULL)
-	{
-		printf("Error\n");
-
-		return (0);
-	}
-
 	temp = h;
 	while (temp != NULL)
 	{
